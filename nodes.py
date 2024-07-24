1ab2f3f4d6cb39b5a8cea65184bbb05fe3f8b56d
kijai <40791699+kijai@users.noreply.github.com>
2024-07-24 20:49:52 +0300

    fix Comfy model loader

--- nodes.py
+++ nodes.py
@@ -746,7 +746,7 @@ class ADMD_ComfyModelLoader:
             original_config = OmegaConf.load(os.path.join(script_directory, f"configs/v1-inference.yaml"))
             ad_unet_config = OmegaConf.load(os.path.join(script_directory, f"configs/ad_unet_config.yaml"))
 
-            from diffusers.loaders.single_file_utils import (convert_ldm_vae_checkpoint, convert_ldm_unet_checkpoint, create_text_encoder_from_ldm_clip_checkpoint, create_vae_diffusers_config, create_unet_diffusers_config)
+            from .single_file_utils import (convert_ldm_vae_checkpoint, convert_ldm_unet_checkpoint, create_text_encoder_from_ldm_clip_checkpoint, create_vae_diffusers_config, create_unet_diffusers_config)
 
             tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
 
